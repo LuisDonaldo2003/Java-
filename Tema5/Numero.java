e2c0f2f4e28ccff0fd0a90b461e9a62c5b007710
Luis Donaldo López Martínez <105167486+LuisDonaldo2003@users.noreply.github.com>
2022-06-13 21:15:39 -0500

    Create Numero.java

---
+++ Tema5/Numero.java
@@ -0,0 +1,38 @@
+package Archivo;
+
+public class Numero {
+    private int cifra;
+    private int totalDigito;
+
+
+    public Numero(int cifra){
+        this.cifra = cifra;
+        this.calcularCifra();
+    }
+
+    public int getCifra(){
+        return cifra;
+    }
+
+    public int getTotalDigito(){
+        return totalDigito;
+    }
+
+
+    public void calcularCifra(){
+        if ((this.cifra >=0 && this.cifra < 10)){
+            this.totalDigito = 1;
+        } else if (this.cifra >=10 && this.cifra < 100){
+            this.totalDigito = 2;
+        } else if(this.cifra >=100 && this.cifra < 1000){
+            this.totalDigito = 3;
+        } else if (this.cifra >=1000 && this.cifra < 10000){
+            this.totalDigito = 4;
+        }
+    }
+
+    public static void main(String[] args){
+        Numero numero = new Numero(60);
+        System.out.println(numero.getTotalDigito());
+    }
+}
